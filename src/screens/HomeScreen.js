74eba554a83d2a4270aceffd4d42c6282ccadce9
Joyce Quach <33106214+jtquach1@users.noreply.github.com>
2020-06-15 16:30:44 -0400

    Change whitespace

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -4,49 +4,49 @@ import Daughter from "../avatars/Daughter";
 import Mother from "../avatars/Mother";
 
 const HomeScreen = ({ navigation }) => {
-  const makeButton = (title, isMother) => {
+    const makeButton = (title, isMother) => {
+        return (
+            <Button 
+                onPress={() => {
+                    navigation.navigate("Menu", {
+                        flag: isMother,
+                    });
+                }}
+                title={title} 
+            />
+        );
+    }
+
+
+    // Generate scenarios based on id
     return (
-      <Button 
-        onPress={() => {
-          navigation.navigate("Menu", {
-            flag: isMother,
-          });
-        }}
-        title={title} 
-      />
+        <ScrollView>
+                <View style={[styles.column, styles.margin]}>
+                        <View style={styles.margin}>
+                                {makeButton("Play as mother", true)}
+                                <Mother />
+                        </View>
+                        <View style={styles.margin}>
+                                {makeButton("Play as daughter", false)}
+                                <Daughter />
+                        </View>
+                </View>
+        </ScrollView>
     );
-  }
-
-
-  // Generate scenarios based on id
-  return (
-    <ScrollView>
-        <View style={[styles.column, styles.margin]}>
-            <View style={styles.margin}>
-                {makeButton("Play as mother", true)}
-                <Mother />
-            </View>
-            <View style={styles.margin}>
-                {makeButton("Play as daughter", false)}
-                <Daughter />
-            </View>
-        </View>
-    </ScrollView>
-  );
 };
 
 const styles = StyleSheet.create({
-  margin: {
-    marginVertical: 20,
-  },
-  row: {
-    alignItems: 'center',
-    flexDirection: 'row',
-  },
-  column: {
-    alignItems: 'center',
-    flexDirection: 'column',
-  },
+    margin: {
+        marginVertical: 20,
+    },
+    row: {
+        alignItems: 'center',
+        flexDirection: 'row',
+    },
+    column: {
+        alignItems: 'center',
+        flexDirection: 'column',
+    },
 });
 
 export default HomeScreen;
